26f934e23befb1b04bac2f7046c830a7b2ef3e71
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-03-20 09:04:22 +0900

    fix : base response

--- hertz/src/main/kotlin/com/bestswlkh0310/hertz/global/base/BaseResponse.kt
+++ hertz/src/main/kotlin/com/bestswlkh0310/hertz/global/base/BaseResponse.kt
@@ -1,7 +1,22 @@
 package com.bestswlkh0310.hertz.global.base
 
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+
 data class BaseResponse<T>(
     val status: Int,
     val message: String,
     val data: T = Unit as @ParameterName(name = "data") T
-)
+) {
+    companion object {
+        fun <T> ok(data: T, headers: HttpHeaders? = null, httpStatus: HttpStatus = HttpStatus.OK): ResponseEntity<Any> {
+            val body = BaseResponse(
+                status = httpStatus.value(),
+                message = httpStatus.reasonPhrase,
+                data = data
+            )
+            return ResponseEntity(body, headers, httpStatus)
+        }
+    }
+}
