82f6c72dfbaa67b58cb23711990458760e6b4aa0
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-03-25 19:26:21 +0900

    test : save music

--- hertz/src/test/kotlin/com/bestswlkh0310/hertz/HertzApplicationTests.kt
+++ hertz/src/test/kotlin/com/bestswlkh0310/hertz/HertzApplicationTests.kt
@@ -2,6 +2,9 @@ package com.bestswlkh0310.hertz
 
 import com.bestswlkh0310.hertz.domain.email.api.dto.EmailMessage
 import com.bestswlkh0310.hertz.domain.email.core.service.EmailService
+import com.bestswlkh0310.hertz.domain.music.core.model.MusicEntity
+import com.bestswlkh0310.hertz.domain.music.core.repository.MusicRepository
+import com.bestswlkh0310.hertz.domain.music.core.service.MusicService
 import com.bestswlkh0310.hertz.global.common.EmailCodeUtil
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -13,6 +16,9 @@ class HertzApplicationTests {
 
 	@Autowired
 	lateinit var emailService: EmailService
+
+	@Autowired
+	lateinit var musicRepository: MusicRepository
 
 	@Test
 	fun musics() {
@@ -38,5 +44,16 @@ class HertzApplicationTests {
 		println(EmailCodeUtil.generateEmailCode(8))
 		println(EmailCodeUtil.generateEmailCode(10))
 		println(EmailCodeUtil.generateEmailCode(100))
+	}
+
+	@Test
+	fun addMusic() {
+		val e = MusicEntity(
+			music = "백예린의 _예뻤어_ AI cover Ver.  DAY6 - _You Were Beautiful_ Yerin Baek AI cover ver. [lyrics].mp3",
+			author = "ai",
+		)
+		for (i in 0..<10) {
+			musicRepository.save(e)
+		}
 	}
 }
