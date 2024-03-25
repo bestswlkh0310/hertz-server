232050822ad8a8ce820224891388bca7cde09f98
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-03-25 21:48:15 +0900

    feat : handle 403

--- hertz/src/main/kotlin/com/bestswlkh0310/hertz/global/config/SecurityConfig.kt
+++ hertz/src/main/kotlin/com/bestswlkh0310/hertz/global/config/SecurityConfig.kt
@@ -2,6 +2,7 @@ package com.bestswlkh0310.hertz.global.config
 
 import com.bestswlkh0310.hertz.domain.user.core.service.UserService
 import com.bestswlkh0310.hertz.global.common.Api
+import com.bestswlkh0310.hertz.global.exception.ForbiddenAuthenticationEntryPoint
 import com.bestswlkh0310.hertz.global.filter.JwtExceptionFilter
 import com.bestswlkh0310.hertz.global.filter.JwtTokenFilter
 import com.bestswlkh0310.hertz.global.jwt.JwtTokenUtil
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.AuthenticationEntryPoint
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
@@ -21,9 +23,9 @@ class SecurityConfig(
     private val userService: UserService,
     private val jwtTokenUtil: JwtTokenUtil,
     private val jwtTokenFilter: JwtTokenFilter,
-    private val jwtExceptionFilter: JwtExceptionFilter
+    private val jwtExceptionFilter: JwtExceptionFilter,
+    private val forbiddenAuthenticationEntryPoint: ForbiddenAuthenticationEntryPoint
 ) {
-
 
     @Bean
     fun securityFilterChain(httpSecurity: HttpSecurity): SecurityFilterChain {
@@ -50,10 +52,14 @@ class SecurityConfig(
                     "${Api.User.PATH}${Api.User.SIGN_UP}",
                     "${Api.User.PATH}${Api.User.SIGN_IN}",
                     "${Api.User.PATH}${Api.User.REFRESH}",
-                    "${Api.User.PATH}${Api.User.SEND_EMAIL_CODE}"
+                    "${Api.User.PATH}${Api.User.SEND_EMAIL_CODE}",
+                    "/error"
                 ).permitAll()
                     .anyRequest()
                     .authenticated()
+            }
+            .exceptionHandling {
+                it.authenticationEntryPoint(forbiddenAuthenticationEntryPoint)
             }
             .build()
     }
--- hertz/src/main/kotlin/com/bestswlkh0310/hertz/global/exception/CustomExceptionHandler.kt
+++ hertz/src/main/kotlin/com/bestswlkh0310/hertz/global/exception/CustomExceptionHandler.kt
@@ -3,6 +3,7 @@ package com.bestswlkh0310.hertz.global.exception
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.client.HttpClientErrorException.Forbidden
 
 
 @ControllerAdvice
@@ -10,6 +11,12 @@ class CustomExceptionHandler {
 
     @ExceptionHandler(CustomException::class)
     protected fun handleCustomException(e: CustomException): ResponseEntity<ErrorResponse> {
-        return ErrorResponse.fromErrorCode(e.errorCode)
+        return ErrorResponse.makeResponseEntity(e.errorCode)
     }
+
+    @ExceptionHandler(Forbidden::class)
+    fun handleForbiddenException() = ErrorResponse.makeResponseEntity(ErrorCode.FORBIDDEN)
+
+    @ExceptionHandler(Exception::class)
+    fun handleUnexpectedException() = ErrorResponse.makeResponseEntity(ErrorCode.INTERNAL_SERVER_ERROR)
 }
