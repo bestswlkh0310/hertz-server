74279790e0a576f40994e205c05535b8e9dd2a5e
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-03-25 18:10:32 +0900

    feat : create playlist dtoes

---
+++ hertz/src/main/kotlin/com/bestswlkh0310/hertz/domain/playlist/api/dto/request/AddMusicRequest.kt
@@ -0,0 +1,6 @@
+package com.bestswlkh0310.hertz.domain.playlist.api.dto.request
+
+data class AddMusicRequest(
+    val playlistId: Int,
+    val musicId: Int
+)
---
+++ hertz/src/main/kotlin/com/bestswlkh0310/hertz/domain/playlist/api/dto/request/DeleteMusicPlaylistRequest.kt
@@ -0,0 +1,6 @@
+package com.bestswlkh0310.hertz.domain.playlist.api.dto.request
+
+data class DeleteMusicPlaylistRequest(
+    val playlistId: Int,
+    val musicId: Int
+)
