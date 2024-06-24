b7f51bb6392e2380631f6e20ee8b5d03573ed034
hhhello <hhhello0507@dgsw.hs.kr>
2024-06-24 12:25:40 +0900

    feat: UserWebAdapter

---
+++ src/main/kotlin/com/bestswlkh0310/hertz/presentation/user/UserWebAdapter.kt
@@ -0,0 +1,22 @@
+package com.bestswlkh0310.hertz.presentation.user
+
+import com.bestswlkh0310.hertz.core.user.service.UserService
+import com.bestswlkh0310.hertz.infra.common.Api
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping(Api.User.PATH)
+class UserWebAdapter(
+    private val userService: UserService
+) {
+
+    @GetMapping(Api.User.GET)
+    fun get(
+        @PathVariable("id") id: Int
+    ) = userService.get(id)
+        .let { ResponseEntity.ok(it) }
+}
